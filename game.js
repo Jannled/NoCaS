89d0c3def80241c6b94a84809a5f0bccf8b6da13
Jannled <Jannled@live.de>
2018-11-02 20:17:25 +0100

    Nocas is now index.html

---
+++ game.js
@@ -0,0 +1,8 @@
+
+main();
+
+function main()
+{
+	document.getElementById("JavaScriptErr").innerHTML = '<canvas id="glcanvas" width="400" height="400" onload="newSize();">Looks like your browser does not support canvas!</canvas>';
+}
+
