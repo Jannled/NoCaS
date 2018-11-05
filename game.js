e1d416ccf8b6ed5ec42a451549c34943556cbcbb
Jannled <Jannled@live.de>
2018-11-05 17:58:28 +0100

    Error while displaying canvas
    I made some changes that broke the rendering of the canvas. It is now showing up properly but still no images are loaded due to CORS...

--- game.js
+++ game.js
@@ -3,6 +3,6 @@ main();
 
 function main()
 {
-	document.getElementById("JavaScriptErr").innerHTML = '<canvas id="glcanvas" width="400" height="400" onload="newSize();">Looks like your browser does not support canvas!</canvas>';
+	document.getElementById("JavaScriptErr").innerHTML = '<canvas id="glcanvas" width="400" height="400" onload="newSize();">Looks like your browser does not support the canvas tag!</canvas>';
 }
 
