c2c8b99bf30f3e31f6e7d13fe704e1b4beaa9c3b
Jannled <Jannled@live.de>
2018-12-04 21:49:22 +0100

    OBJ converter runs out of memory

--- JannledModelFormat/convert.js
+++ JannledModelFormat/convert.js
@@ -1,67 +1,74 @@
 
 function convertFromObj(objContent)
 {
-  var modelName = "--NULL--";
-  var vertices = [];
-  var normals = [];
-  var texCoords = [];
-  var faces = [];
+    var modelName = "--NULL--";
+    var vertices = [];
+    var normals = [];
+    var texCoords = [];
+    var faces = [];
 
-  var lines = objContent.split('\n');
-  for(var i=0; i<lines.length; i++)
-  {
-    lines[i] = lines[i].trim();
-    var key = lines[i].split(' ');
-    switch(key[0])
+    var lines = objContent.split('\n');
+    for(var i=0; i<lines.length; i++)
     {
-      case "o":
-        modelName = lines[i].slice(1).trim();
-        break;
-
-      case "v":
-        vertices[vertices.length] = key[1];
-        vertices[vertices.length] = key[2];
-        vertices[vertices.length] = key[3];
-        break;
-
-      case "vn":
-        normals[normals.length] = key[1];
-        normals[normals.length] = key[1];
-        normals[normals.length] = key[1];
-        break;
-
-      case "f":
-        var triplet = lines[i].slice(1).trim().split(' ');
-        for(var i=0; i<3; i++)
+        lines[i] = lines[i].trim();
+        var key = lines[i].split(' ');
+        switch(key[0])
         {
-          var part = triplet.split('/');
-          switch(part.length)
-          {
-            case 1:
-              faces[faces.length] = triplet;
-              break;
+            case "o":
+                modelName = lines[i].slice(1).trim();
+                break;
 
-            case 2:
-              break;
+            case "v":
+                vertices[vertices.length] = key[1];
+                vertices[vertices.length] = key[2];
+                vertices[vertices.length] = key[3];
+                break;
 
-            case 3:
-              faces[faces.length] = part[0];
-              break;
-          }
-          break;
-        }
+            case "vn":
+                normals[normals.length] = key[1];
+                normals[normals.length] = key[1];
+                normals[normals.length] = key[1];
+                break;
 
-      default:
-        console.error("Undefined key: " + key[0] + ", in line " + i + "!");
-        break;
+            case "f":
+                var triplet = lines[i].slice(1).trim().split(' ');
+                for(var i=0; i<3; i++)
+                {
+                    var part = triplet[i].split('/');
+                    switch(part.length)
+                    {
+                        case 1:
+                            faces[faces.length] = triplet;
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            faces[faces.length] = part[0];
+                            break;
+                    }
+                    break;
+                }
+			case "#": break;
+
+			case "mtllib": break;
+
+			case "usemtl": break;
+
+			case "s": break;
+
+        	default:
+                	console.error("Undefined key: " + key[0] + ", in line " + i + "!");
+                break;
+		}
     }
-  }
 
-  return {
-	  modelName,
-	  vertices,
-	  normals,
-	  texCoords,
-	  faces
-  };
+    return {
+	    modelName,
+	    vertices,
+	    normals,
+	    texCoords,
+	    faces
+	  };
 }
