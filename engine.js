c6f47c8ea76f9c57839130b18b7a7ef729894e52
jannled <jannled@live.de>
2019-03-05 09:24:31 +0100

    Have to fix fullscreen, implemented FPS independent gameloop

--- engine.js
+++ engine.js
@@ -70,20 +70,22 @@ const fsSource = `
 		FragColor = vec4(texelColor.rgb * (ambientLight + diffuse + specular), 1.0f);
 	}`;
 
-var gl; /* Bad approach */
+var gl;
+var engine; /* Bad approach */
 
 class Engine
 {
 	/**
-	 * @param{HTMLElement} canvasElement
+	 * @param{HTMLElement} canvasElement The HTML Element of the canvas
 	 */
 	constructor(canvasElement, displayRatio)
 	{
 		var currentTime = 0.0;
+		engine = this;
 
 		if(typeof displayRatio !== 'number') this.displayRatio = 16 / 9; else this.displayRatio = displayRatio;
 		this.canvas = canvasElement;
-		if(canvasElement instanceof HTMLElement) init();
+		if(canvasElement instanceof HTMLElement) this.init(this.canvas); else console.error("Failed to initialize engine. Please pass an HTML Element as first parameter!");
 	}
 
 	init(canvas)
@@ -112,7 +114,7 @@ class Engine
 		//WebGL is running
 		if(wgl != null) wgl.style.color = "#00FF00";
 
-		this.newSize();
+		engine.newSize();
 
 		//Print out OpenGL Version
 		console.log(gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
@@ -143,56 +145,139 @@ class Engine
 		};
 
 		var then = 0;
+		const logicTicks = 0.05;
+		var timeSinceUpdate = logicTicks;
+		var fpsElement = document.getElementById("FPS");
 
 		//Update canvas size
 		window.scrollBy(0,1); //Safari fix
-		window.addEventListener('resize', this.newSize);
+		window.addEventListener('resize', engine.newSize);
 
 		/** Called by requestAnimationFrame */
-		function draw(timestamp)
+		function renderLoop(timestamp)
 		{
 			timestamp *= 0.001;	// convert to seconds
 			const deltaTime = timestamp - then;
 			then = timestamp;
 
-			activeScene.update(deltaTime);
-			activeScene.render(programInfo, deltaTime);
-			requestAnimationFrame(draw);
+			timeSinceUpdate = timeSinceUpdate + deltaTime;
+
+			if(timeSinceUpdate > logicTicks)
+			{
+				timeSinceUpdate = timeSinceUpdate - logicTicks;
+				engine.activeScene.update(deltaTime);
+
+				if(fpsElement instanceof HTMLElement) fpsElement.innerHTML = Math.round(1/(deltaTime));
+			}
+
+			engine.activeScene.render(programInfo, deltaTime);
+			requestAnimationFrame(renderLoop);
 		}
-		requestAnimationFrame(draw);
+		requestAnimationFrame(renderLoop);
+	}
+
+	/**
+	 *  Uploads the model to the GPU
+	 */ //positions, normals, texCoords, indices, textureUrl, position, rotation, scale
+	 loadModel(model)
+	 {
+	 	//Build the Vertices buffer (Attrib 0)
+	 	const positionBuffer = gl.createBuffer();
+	 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.vertices), gl.STATIC_DRAW);
+
+	 	//Build the Normals buffer (Attrib 1)
+	 	const normalBuffer = gl.createBuffer();
+	 	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+	 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.normals), gl.STATIC_DRAW);
+
+	 	//Build the texture coords buffer (Attrib 2)
+	 	const textureCoordBuffer = gl.createBuffer();
+	 	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+	 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.uvs), gl.STATIC_DRAW);
+
+	 	// Build the element array buffer (Attrib 3)
+	 	const indexBuffer = gl.createBuffer();
+	 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(model.indices), gl.STATIC_DRAW);
+
+		// Load the Texture data
+	 	const textureBuffer = gl.createTexture();
+	 	gl.bindTexture(gl.TEXTURE_2D, textureBuffer);
+
+	 	const level = 0;
+	 	const internalFormat = gl.RGBA;
+	 	const width = 1;
+	 	const height = 1;
+	 	const border = 0;
+	 	const srcFormat = gl.RGBA;
+	 	const srcType = gl.UNSIGNED_BYTE;
+	 	const pixel = new Uint8Array([255, 0, 255, 255]); //Color of unloaded texture
+	 	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+	 								width, height, border, srcFormat, srcType,
+	 								pixel);
+
+	 	const image = new Image();
+	 	image.onload = function() {
+	 		gl.bindTexture(gl.TEXTURE_2D, textureBuffer);
+	 		gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+	 									srcFormat, srcType, image);
+
+	 		if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+	 			 gl.generateMipmap(gl.TEXTURE_2D);
+	 		} else {
+	 			 // wrapping to clamp to edge
+	 			 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	 			 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	 			 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	 		}
+	 	};
+	 	image.src = model.textureUrl;
+
+	 	model.ogldata.positionBuffer = positionBuffer;
+	 	model.ogldata.normalBuffer = normalBuffer;
+	 	model.ogldata.textureCoordBuffer = textureCoordBuffer;
+	 	model.ogldata.indexBuffer = indexBuffer;
+		model.ogldata.textureBuffer = textureBuffer;
+
+		return model; //For convenience
+	 }
+
+
+	/**
+	 * Uses XHR to download a model file which will then be parsed and loaded into the scene
+	 * @param fileUrl Path of the jmf file to be downloaded
+	*/
+	loadModelFromUrl(fileUrl, imageUrl, position, rotation, scale)
+	{
+		var activeScene = engine.activeScene;
+
+		requestFile(fileUrl, function(data, status)
+		{
+			console.log('Request: "' + fileUrl + '" [' + status + "].")
+			var mesh = convertFromJMF(data);
+			var model = new Model(mesh.name, mesh.vertices, mesh.normals, mesh.uvs, mesh.indices, imageUrl, position, rotation, scale);
+			engine.activeScene.loadModelToScene(model);
+		});
 	}
 
 	/** Handle window y */
 	newSize()
 	{
-		var twidth = canvas.parentElement.offsetWidth;
-		var theight = Math.round(twidth / displayRatio);
+		var twidth = engine.canvas.parentElement.offsetWidth;
+		var theight = Math.round(twidth / engine.displayRatio);
 
 		console.log("Canvas resized to " + twidth + "x" + theight);
-		canvas.width = twidth;
-		canvas.height = theight;
-		gl.viewport(0, 0, canvas.width, canvas.height);
-	}
-
-	/** Make game fullscreen */
-	goFullscreen()
-	{
-		var gameArea = document.getElementById("gamearea");
-		if (gameArea.requestFullscreen) {
-			gameArea.requestFullscreen();
-		} else if (gameArea.mozRequestFullScreen) { /* Firefox */
-			gameArea.mozRequestFullScreen();
-		} else if (gameArea.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-			gameArea.webkitRequestFullscreen();
-		} else if (gameArea.msRequestFullscreen) { /* IE/Edge */
-			gameArea.msRequestFullscreen();
-		}
+		engine.canvas.width = twidth;
+		engine.canvas.height = theight;
+		gl.viewport(0, 0, engine.canvas.width, engine.canvas.height);
 	}
 
 	lockMouse()
 	{
 		canvas.requestPointerLock();
 	}
+
 }
 
 /**
@@ -301,7 +386,7 @@ class Scene
 	constructor()
 	{
 		this.models = [];
-		this.ambientLight = Float32Array.from([0, 0, 0]);
+		this.ambientLight = Float32Array.from([0.0, 0.0, 0.0]);
 		this.cameraPosition = new Float32Array(3);
 	}
 
@@ -339,14 +424,32 @@ class Scene
 		programInfo.program.setVec3("lightPos", [5, 5, 10]);
 		programInfo.program.setVec3("lightColor", [1, 1, 1]);
 		programInfo.program.setVec3("viewPos", this.cameraPosition);
-		// uniform vec3 ambientLight;
-		// uniform vec3 lightPos;
-		// uniform vec3 lightColor;
-		// uniform vec3 viewPos;
-
 		for(var i=0; i<this.models.length; i++)
 		{
 			this.models[i].render(programInfo, projectionMatrix);
+		}
+	}
+
+	loadModelToScene(model)
+	{
+		if(typeof model === 'undefined')
+		{
+			console.warn("Model is undefined, perhaps something went wrong while loading it?");
+			return;
+		}
+		if((model.position instanceof Float32Array) && (model.position.length == 3) &&
+			(model.rotation instanceof Float32Array) && (model.rotation.length == 3) &&
+			(model.scale instanceof Float32Array) && (model.scale.length == 3) &&
+			(model.vertices instanceof Float32Array) && (model.normals instanceof Float32Array) &&
+			(model.uvs instanceof Float32Array) && (model.indices instanceof Uint32Array))
+		{
+			console.info("Loading model with " + (model.vertices.length/3) + " vertices, " + (model.indices.length/3) + ' indices with the texture url: "' + model.texture + '".');
+			this.models[this.models.length] = engine.loadModel(model);
+		}
+		else
+		{
+			console.warn("Failed to load model, invalid params!");
+			return;
 		}
 	}
 }
@@ -483,104 +586,26 @@ class Model
 	}
 }
 
-
-
-main();
-
-/** Main Method */
-function main()
+/** Make specified HTMLElement fullscreen
+ * @param{HTMLElement} element The HTML element to request fullscreen for.
+ */
+function goFullscreen(element)
 {
-
+	if(!element instanceof HTMLElement)
+		console.error("Please specifie a valid HTML element (" + typeof element + ")!");
+	if (element.requestFullscreen) {
+		element.requestFullscreen();
+	} else if (element.mozRequestFullScreen) { /* Firefox */
+		element.mozRequestFullScreen();
+	} else if (element.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+		element.webkitRequestFullscreen();
+	} else if (element.msRequestFullscreen) { /* IE/Edge */
+		element.msRequestFullscreen();
+	}
 }
 
-/**
- *  Uploads the model to the GPU
- */ //positions, normals, texCoords, indices, textureUrl, position, rotation, scale
- function loadModel(gl, model)
- {
- 	//Build the Vertices buffer (Attrib 0)
- 	const positionBuffer = gl.createBuffer();
- 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
- 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.vertices), gl.STATIC_DRAW);
-
- 	//Build the Normals buffer (Attrib 1)
- 	const normalBuffer = gl.createBuffer();
- 	gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
- 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.normals), gl.STATIC_DRAW);
-
- 	//Build the texture coords buffer (Attrib 2)
- 	const textureCoordBuffer = gl.createBuffer();
- 	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
- 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.uvs), gl.STATIC_DRAW);
-
- 	// Build the element array buffer (Attrib 3)
- 	const indexBuffer = gl.createBuffer();
- 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
- 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(model.indices), gl.STATIC_DRAW);
-
-	// Load the Texture data
- 	const textureBuffer = gl.createTexture();
- 	gl.bindTexture(gl.TEXTURE_2D, textureBuffer);
-
- 	const level = 0;
- 	const internalFormat = gl.RGBA;
- 	const width = 1;
- 	const height = 1;
- 	const border = 0;
- 	const srcFormat = gl.RGBA;
- 	const srcType = gl.UNSIGNED_BYTE;
- 	const pixel = new Uint8Array([255, 0, 255, 255]); //Color of unloaded texture
- 	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
- 								width, height, border, srcFormat, srcType,
- 								pixel);
-
- 	const image = new Image();
- 	image.onload = function() {
- 		gl.bindTexture(gl.TEXTURE_2D, textureBuffer);
- 		gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
- 									srcFormat, srcType, image);
-
- 		if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
- 			 gl.generateMipmap(gl.TEXTURE_2D);
- 		} else {
- 			 // wrapping to clamp to edge
- 			 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
- 			 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
- 			 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
- 		}
- 	};
- 	image.src = model.textureUrl;
-
- 	model.ogldata.positionBuffer = positionBuffer;
- 	model.ogldata.normalBuffer = normalBuffer;
- 	model.ogldata.textureCoordBuffer = textureCoordBuffer;
- 	model.ogldata.indexBuffer = indexBuffer;
-	model.ogldata.textureBuffer = textureBuffer;
-
-	return model; //For convenience
- }
-
-function loadModelToScene(gl, model)
-{
-	if(typeof model === 'undefined')
-	{
-		console.warn("Model is undefined, perhaps something went wrong while loading it?");
-		return;
-	}
-	if((model.position instanceof Float32Array) && (model.position.length == 3) &&
-		(model.rotation instanceof Float32Array) && (model.rotation.length == 3) &&
-		(model.scale instanceof Float32Array) && (model.scale.length == 3) &&
-		(model.vertices instanceof Float32Array) && (model.normals instanceof Float32Array) &&
-		(model.uvs instanceof Float32Array) && (model.indices instanceof Uint32Array))
-	{
-		console.info("Loading model with " + (model.vertices.length/3) + " vertices, " + (model.indices.length/3) + ' indices with the texture url: "' + model.texture + '".');
-		activeScene.models[activeScene.models.length] = loadModel(gl, model);
-	}
-	else
-	{
-		console.warn("Failed to load model, invalid params!");
-		return;
-	}
+function isPowerOf2(value) {
+	return (value & (value - 1)) == 0;
 }
 
 /**
@@ -603,25 +628,6 @@ function requestFile(fileUrl, /*Function*/runOnLoad)
 	}
 	xhr.open("GET", fileUrl);
 	xhr.send();
-}
-
-/**
- * Uses JQuery to download a model file which will then be parsed and loaded into the scene
- * @param fileUrl Path of the jmf file to be downloaded
-*/
-function loadModelFromUrl(fileUrl, imageUrl, position, rotation, scale)
-{
-	requestFile(fileUrl, function(data, status)
-	{
-		console.log('Request: "' + fileUrl + '" [' + status + "].")
-		var mesh = convertFromJMF(data);
-		var model = new Model(mesh.name, mesh.vertices, mesh.normals, mesh.uvs, mesh.indices, imageUrl, position, rotation, scale);
-		loadModelToScene(gl, model);
-	});
-}
-
-function isPowerOf2(value) {
-	return (value & (value - 1)) == 0;
 }
 
 
--- game.js
+++ game.js
@@ -2,7 +2,8 @@ main();
 
 function main()
 {
-	loadModelFromUrl('models/GenericCube.jmf', 'cubetexture.png', [2, 0, -6], [0.5, 1, 0]);
-	loadModelFromUrl('models/Sphere.jmf', '', [-2, -1, -6], [0.5, 1, 0], [0.1, 0.1, 0.1]);
+	var engine = new Engine(document.getElementById('glcanvas'));
+	engine.loadModelFromUrl('models/GenericCube.jmf', 'cubetexture.png', [2, 0, -6], [0.5, 1, 0]);
+	engine.loadModelFromUrl('models/Sphere.jmf', '', [-2, -1, -6], [0.5, 1, 0], [0.1, 0.1, 0.1]);
 	//lockMouse();
 }
